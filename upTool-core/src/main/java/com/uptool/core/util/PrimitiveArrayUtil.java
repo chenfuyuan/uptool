cb601f23c9a0595542a1dee7ec86954023f471fe
chenfuyuan <chenfuyuan0713@163.com>
2021-01-03 22:44:44 +0800

    '初始化'

---
+++ upTool-core/src/main/java/com/uptool/core/util/PrimitiveArrayUtil.java
@@ -0,0 +1,226 @@
+package com.uptool.core.util;
+
+/**
+ * @Description: 原始数组工具类
+ * @Author chenfuyuan
+ * @Date 2020/12/29 18:02
+ */
+public class PrimitiveArrayUtil {
+
+    /**
+     * 数组中元素未找到的下标，值为-1
+     */
+    public static final int INDEX_NOT_FOUND = -1;
+
+    // ---------------------------------------------------------------------- isEmpty
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(long[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(int[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(short[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(char[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(byte[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(double[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(float[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(boolean[] array) {
+        return array == null || array.length == 0;
+    }
+
+    // ---------------------------------------------------------------------- isNotEmpty
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(long[] array) {
+        return false == isEmpty(array);
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(int[] array) {
+        return false == isEmpty(array);
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(short[] array) {
+        return false == isEmpty(array);
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(char[] array) {
+        return false == isEmpty(array);
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(byte[] array) {
+        return false == isEmpty(array);
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(double[] array) {
+        return false == isEmpty(array);
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(float[] array) {
+        return false == isEmpty(array);
+    }
+
+    /**
+     * 数组是否为非空
+     *
+     * @param array 数组
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(boolean[] array) {
+        return false == isEmpty(array);
+    }
+
+
+    /**
+     * 生成一个新的重新设置大小的数组<br>
+     * 调整大小后拷贝原数组到新数组下。扩大则占位前N个位置，其它位置补充0，缩小则截断
+     *
+     * @param bytes   原数组
+     * @param newSize 新的数组大小
+     * @return 调整后的新数组
+     * @since 4.6.7
+     */
+    public static byte[] resize(byte[] bytes, int newSize) {
+        if (newSize < 0) {
+            return bytes;
+        }
+
+        final byte[] result = new byte[newSize];
+        if (newSize > 0 && isNotEmpty(bytes)) {
+            System.arraycopy(bytes, 0, result, 0, Math.min(bytes.length, newSize));
+        }
+
+        return result;
+    }
+
+    /**
+     * 将多个数组合并在一起<br>
+     * 忽略null的数组
+     *
+     * @param arrays 数组集合
+     * @return 合并后的数组
+     * @since 4.6.9
+     */
+    public static byte[] addAll(byte[]... arrays) {
+        if (arrays.length == 1) {
+            return arrays[0];
+        }
+
+        int length = 0;
+
+        final byte[] result = new byte[length];
+        for (byte[] array : arrays) {
+            if (null != array) {
+                System.arraycopy(array, 0, result, length, array.length);
+            }
+        }
+        return result;
+    }
+
+}

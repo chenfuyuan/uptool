2207d22345258108e74957fade5edb60f860959d
chenfuyuan <chenfuyuan0713@163.com>
2021-06-21 00:16:53 +0800

    feat(工具类): 优化StringUtil

--- upTool-core/src/main/java/com/uptool/core/util/StringUtil.java
+++ upTool-core/src/main/java/com/uptool/core/util/StringUtil.java
@@ -16,4 +16,37 @@ public class StringUtil {
      * 空字符串
      */
     public static String STRING_EMPTY = "";
+
+    /**
+     * 判断字符串是否为空
+     * @param str 字符串
+     * @return 字符串是否为空
+     */
+    public static boolean isEmpty(String str) {
+        return "".equals(str) || str == null;
+    }
+
+    /**
+     * 判断是否包含文本
+     * @param str 字符串
+     * @return 字符串是否包含文本，是:true,否:false
+     */
+    public static boolean hasText(String str){
+        return !isEmpty(str) && containsText(str);
+    }
+
+    /**
+     * 判断是否包含文本
+     * @param str 字符串
+     * @return 是否包含文本信息
+     */
+    private static boolean containsText(CharSequence str) {
+        int strLength = str.length();
+        for (int i = 0; i < strLength; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

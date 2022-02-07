60ff8a052e37b5b495d8cd863806d1bfa93e1864
chenfuyuan <chenfuyuan0713@163.com>
2022-02-07 17:45:51 +0800

    feat(SortUtil): 增加方法compareNullToZero，当两个Integer类型的对象进行比较时，如果对象为空，则默认为0，参与比较

--- upTool-core/src/main/java/com/uptool/core/util/SortUtil.java
+++ upTool-core/src/main/java/com/uptool/core/util/SortUtil.java
@@ -14,6 +14,16 @@ public class SortUtil {
     public static final int COMPARETO_EQUAL = 0;
 
     /**
+     * 比较值 num1 < num2 标识
+     */
+    public static final int COMPARETO_LESS = -1;
+
+    /**
+     * 比较值 num1 > num2 标识
+     */
+    public static final int COMPARETO_MORE = 1;
+
+    /**
      * 两则进行比较，判断obj1是否小于obj2
      * @param obj1 比较对象
      * @param obj2 比较对象
@@ -21,6 +31,31 @@ public class SortUtil {
      */
     public static boolean less(Comparable obj1, Comparable obj2) {
         return obj1.compareTo(obj2) < COMPARETO_EQUAL;
+    }
+
+    public static int compare(Comparable obj1, Comparable obj2) {
+        return obj1.compareTo(obj2);
+    }
+
+    /**
+     * 两个Intger对象进行比较，如果该对象为空，则默认为0
+     * @param num1 比较对象1
+     * @param num2 比较对象2
+     * @return
+     */
+    public static int compareNullToZero(Integer num1, Integer num2) {
+        return compareNullToDefault(num1, num2, 0);
+    }
+
+    /**
+     * 两个Ineger对象进行比较，如果该对象为空，则采用默认值
+     * @param num1 比较对象1
+     * @param num2 比较对象2
+     * @param defaultNum 默认值
+     * @return
+     */
+    public static int compareNullToDefault(Integer num1, Integer num2, Integer defaultNum) {
+        return compare(num1 == null ? defaultNum : num1, num2 == null ? defaultNum : num2);
     }
 
     /**
